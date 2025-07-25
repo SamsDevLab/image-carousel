0a0258c04a9427786f197b60760e00566a23310f
Sam Hadfield <sthtech@protonmail.com>
2025-07-25 16:23:23 -0500

    Remove comments

--- src/movements.js
+++ src/movements.js
@@ -1,12 +1,47 @@
 export const movements = () => {
   const carousel = document.querySelector("[data-div = 'carousel']");
 
+  const indicators = document.querySelectorAll("[data-indicator-btn]");
+  const indicatorsArr = Array.from(indicators);
+
+  let activeIndex = 0;
+
   const getCarouselPosition = () => {
     const carouselStyle = window.getComputedStyle(carousel);
     const carouselPosition = parseInt(carouselStyle.left);
 
     return carouselPosition;
   };
+
+  const updateActiveIndex = (position) => {
+    const positionArr = ["0px", "-600px", "-1200px", "-1800px", "-2400px"];
+
+    const index = positionArr.findIndex((value) => value === position);
+    activeIndex = index;
+  };
+
+  const updateActiveSlide = () => {
+    const slideArr = ["beach", "mountains", "sunset", "road", "surfboards"];
+    const activeSlide = slideArr[`${activeIndex}`];
+
+    removeIndicatorStyling();
+
+    const foundIndicator = indicatorsArr.find(
+      (indicator) => indicator.dataset.indicatorBtn === activeSlide,
+    );
+
+    foundIndicator.style.backgroundColor = "blue";
+  };
+
+  const updateCarouselPosition = (newStr) => {
+    carousel.style.left = newStr;
+    updateActiveIndex(newStr);
+    updateActiveSlide();
+  };
+
+  ///////////////////////////
+  // Arrow Buttons Section //
+  ///////////////////////////
 
   const addValueToInt = (parsedInt) => {
     const newInteger = parsedInt + 600;
@@ -27,7 +62,7 @@ export const movements = () => {
 
     if (carouselPosition >= -2400 && carouselPosition < 0) {
       const newStr = addValueToInt(carouselPosition);
-      carousel.style.left = newStr;
+      updateCarouselPosition(newStr);
     }
   };
 
@@ -36,32 +71,69 @@ export const movements = () => {
 
     if (carouselPosition > -2400 && carouselPosition <= 0) {
       const newStr = subtractValueFromInt(carouselPosition);
-      carousel.style.left = newStr;
+      updateCarouselPosition(newStr);
     }
   };
 
-  const addListenerToPrevious = (button) => {
-    button.addEventListener("click", () => previous());
-  };
-
-  const addListenerToNext = (button) => {
-    button.addEventListener("click", () => next());
+  const addListenersToArrowBtns = (arrowBtnsArr) => {
+    arrowBtnsArr.forEach((button) => {
+      if (button.dataset.arrowBtn === "prev") {
+        button.addEventListener("click", () => previous());
+      } else if (button.dataset.arrowBtn === "next") {
+        button.addEventListener("click", () => next());
+      }
+    });
   };
 
   const addFunctionalityToArrowBtns = () => {
     const arrowBtns = document.querySelectorAll("[data-arrow-btn]");
     const arrowBtnsArr = Array.from(arrowBtns);
 
-    arrowBtnsArr.forEach((button) => {
-      if (button.dataset.arrowBtn === "prev") {
-        addListenerToPrevious(button);
-      } else if (button.dataset.arrowBtn === "next") {
-        addListenerToNext(button);
-      }
+    addListenersToArrowBtns(arrowBtnsArr);
+  };
+
+  // -------------------------
+
+  const removeIndicatorStyling = () => {
+    indicatorsArr.forEach((button) => (button.style.backgroundColor = ""));
+  };
+
+  // First Draft:
+  const addListenerToIndicatorBtns = (button) => {
+    if (button.dataset.indicatorBtn === "beach") {
+      button.addEventListener("click", () => {
+        updateCarouselPosition("0px");
+      });
+    } else if (button.dataset.indicatorBtn === "mountains") {
+      button.addEventListener("click", () => {
+        updateCarouselPosition("-600px");
+      });
+    } else if (button.dataset.indicatorBtn === "sunset") {
+      button.addEventListener("click", () => {
+        updateCarouselPosition("-1200px");
+      });
+    } else if (button.dataset.indicatorBtn === "road") {
+      button.addEventListener("click", () => {
+        updateCarouselPosition("-1800px");
+      });
+    } else if (button.dataset.indicatorBtn === "surfboards") {
+      button.addEventListener("click", () => {
+        updateCarouselPosition("-2400px");
+      });
+    }
+  };
+
+  // First Draft:
+  const addFunctionalityToIndicatorBtns = () => {
+    indicatorsArr.forEach((button) => {
+      addListenerToIndicatorBtns(button);
     });
   };
 
+  setInterval(next, 5000);
+
   addFunctionalityToArrowBtns();
+  addFunctionalityToIndicatorBtns();
 };
 
 movements();
